4eab739cdb401fc41d02de83caa47d1220a5c687
zafle <fleutotanne@free.fr>
2024-07-15 11:49:41 +0200

    remove old functions

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -5,9 +5,7 @@ class RecipesPage {
     constructor() {
         this._recipesDatas = [];
 
-        // this.$recipeSection = document.querySelector(".recipes")
         this.$recipesCardsWrapper = document.querySelector(".recipes__cards-wrapper")
-
     }
 
     launchSearchBar() {
@@ -21,28 +19,9 @@ class RecipesPage {
 
     displayRecipeCard(recipe) {
         const Template = new RecipeCard(recipe)
-        // this.$recipeSection.append(Template.createRecipepCard())
-        this.$recipesCardsWrapper.append(Template.createRecipeBootstrapCard())
+        this.$recipesCardsWrapper.append(Template.createRecipeCard())
     }
 
-    // logTags(recipe) {
-
-
-    //     // log tags in tags arrays from TagsData object
-
-    //     // ingredients tags
-    //     recipe.ingredientsTags.forEach(ingredient => {
-    //         this._tagsDatas.ingredientsTags = ingredient
-    //     })
-
-    //     // ustensils tags
-    //     recipe.ustensilsTags.forEach(ustensil => {
-    //         this._tagsDatas.ustensilsTags = ustensil
-    //     })
-
-    //     // appliance tagt
-    //     this._tagsDatas.appliancesTags = recipe.applianceTag
-    // }
     createFiltersTags() {
         this._tagsDatas = new TagsDatas(this._recipesDatas)
         this._tagsDatas.createTags()
@@ -55,35 +34,23 @@ class RecipesPage {
     }
 
     displayContent() {
-        // map recipes datas
         this.mapRecipes()
 
         this._recipesDatas.forEach(recipe => {
-            // display recipes cards
             this.displayRecipeCard(recipe)
-            // log filters tags
-            // this.logTags(recipe)
         })
-        // console.log(this._tagsDatas)
-        // display filters tags
-        // this.displayFiltersTags()
     }
 
     run() {
         this.launchSearchBar()
         this.displayContent()
         this.displayFiltersTags()
-
-        // this._tagsDatas.createTags(this._recipesDatas)
-
     }
 }
 
 function init() {
     const recipesPage = new RecipesPage()
     recipesPage.run()
-
-
 }
 
 init()
--- scripts/templates/recipeCard.js
+++ scripts/templates/recipeCard.js
@@ -8,48 +8,10 @@ class RecipeCard {
         this._recipe = recipe
         this._ingredients = recipe.ingredients
 
-        // this.$card = document.createElement("article")
         this.$cardWrapper = document.createElement("div")
-        // this.$card = document.createElement("div")
     }
 
     createRecipeIngredientsList() {
-        let list = ""
-
-        this._ingredients.forEach(ingredient => {
-            const li =  `
-                <li class="ingredient__item">
-                    <p class="ingredient__name">${ingredient.name}</p>
-                    <p class="ingredient__quantity">${ingredient.quantity}</p>
-                </li>
-            `
-            list += li
-        })
-        return list
-    }
-
-    createRecipeCard() {
-        this.$card.classList.add("recipe")
-        this.$card.dataset.id = this._recipe.id
-
-        this.$card.innerHTML = `
-            <img src="${this._recipe.image}" alt="${this._recipe.name}" class="recipe__image">
-            <span class="recipe__time">${this._recipe.time}</span>
-            <div class="recipe__description">
-                <h2 class="recipe__name">${this._recipe.name}</h2>
-                <h3 class="recipe__subtitle">Recette</h3>
-                <p class="recipe__recipe">${this._recipe.description}</p>
-                <h3 class="recipe__subtitle">Ingrédients</h3>
-                <ul class="ingredient__list">
-                    ${this.createRecipeIngredientsList()}
-                </ul>
-            </div>
-        `
-        return this.$card
-    }
-
-    //  bootstrap
-    createRecipeIngredientsBootstrapList() {
         let list = ""
 
         this._ingredients.forEach(ingredient => {
@@ -64,7 +26,7 @@ class RecipeCard {
         return list
     }
 
-    createRecipeBootstrapCard() {
+    createRecipeCard() {
         this.$cardWrapper.classList.add("col")
         this.$cardWrapper.innerHTML = `
             <div class="card h-100 border-0 rounded-9">
@@ -78,14 +40,12 @@ class RecipeCard {
                     <p class="recipe__recipe card-text lh-sm h-76 fs-2 overflow-hidden text-dark">${this._recipe.description}</p>
                     <h3 class="recipe__subtitle card-subtitle fs-1 text-secondary fw-bold text-uppercase mb-15">Ingrédients</h3>
                     <ul class="ingredient__list list-unstyled row row-cols-2 gx-30 gy-20 mb-0">
-                        ${this.createRecipeIngredientsBootstrapList()}
+                        ${this.createRecipeIngredientsList()}
                     </ul>
 
                 </div>
             </div>
         `
         return this.$cardWrapper
-
-
     }
 }
