5285b936600adb6939ac995852650eeffd9a0089
zafle <fleutotanne@free.fr>
2024-08-05 09:27:17 +0200

    comment tagsDatas Functions

--- scripts/models/autocompleteDatas.js
+++ scripts/models/autocompleteDatas.js
@@ -20,24 +20,30 @@ class AutocompleteDatas {
 
     get autocompleteTags() {
 
-
+        // construct tags datas
         const tagsDatas = new TagsDatas(this._recipes)
-        // tagsDatas.createTagsArrays()
 
         let allTags = []
 
+        // for each type of filter, get tags from tagsDatas, map each array, and push each array into allTags array
         this._filters.forEach( filter => {
             this[`_${filter}Tags`] = this.createTagArray(tagsDatas[`${filter}Tags`], filter)
             allTags.push(this[`_${filter}Tags`])
         })
 
+        // flat alltags array
         this._autocompleteTags = allTags.flat()
-        console.log("this._autocompleteTags")
-        console.log(this._autocompleteTags)
+
         return this._autocompleteTags
     }
 
     createTagArray(array, filter) {
+        /** map the array of tags to to construs a key/value array : filter = filter's name and name = tag's name
+         *
+         * @param {Array} array array of tags
+         * @param {String} filter type of filter
+         *
+         */
 
         const newArray = Array.from(array).map(item => {
             const rObj = {}
--- scripts/models/tagsDatas.js
+++ scripts/models/tagsDatas.js
@@ -2,7 +2,9 @@ class TagsDatas {
     /** Constructor patternfor tags'datas from all recipes
      *
      * @param {Array} recipes RecipeData Objects
+     *
      */
+
     constructor(recipes) {
         this._recipes = recipes
 
@@ -27,12 +29,29 @@ class TagsDatas {
     }
 
     createArray(filter) {
+        /** create an array of uniques tags from filter type
+         *
+         * @param {String} filter type of filter
+         *
+         */
+
+        // map recipes with filter tags and then flat arrays
         const tags = this._recipes.flatMap(recipe => recipe[`${filter}Tags`])
+
+        // transform tag to lower case and capitalize first letter to allow finding duplicates
         const capitalizedTagsArray = tags.map(tag => this.capitalizeFirstLetter(tag))
+
+        // return new array with uniques values
         return Array.from(new Set(capitalizedTagsArray))
     }
 
     capitalizeFirstLetter(string) {
+        /** transform tag to lower case and capitalize first letter
+         *
+         * @param {String} string tag's name
+         *
+         */
+
         return string.charAt(0).toUpperCase() + string.toLowerCase().slice(1)
     }
 }
